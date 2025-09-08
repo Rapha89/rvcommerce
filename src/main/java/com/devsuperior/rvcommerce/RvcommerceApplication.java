c49334b4cedcdcc32b5c0ebd7fbb901c09a812a2
Raphael Vacari <raphaelvacari7@gmail.com>
2025-09-08 11:35:24 -0300

    Order

---
+++ src/main/java/com/devsuperior/rvcommerce/RvcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.rvcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RvcommerceApplication.class, args);
+	}
+
+}
