0c768008673bb06a9e2a17cc76e16453d422fb59
Raphael Vacari <raphaelvacari7@gmail.com>
2025-09-10 13:10:32 -0300

    equal and hashcode

--- src/main/java/com/devsuperior/rvcommerce/entities/Payment.java
+++ src/main/java/com/devsuperior/rvcommerce/entities/Payment.java
@@ -3,6 +3,7 @@ package com.devsuperior.rvcommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.Objects;
 
 @Entity
 @Table(name= "tb_payment")
@@ -52,5 +53,16 @@ public class Payment {
         this.order = order;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
 
+        Payment payment = (Payment) o;
+        return Objects.equals(id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
