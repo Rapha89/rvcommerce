ce26d1547bed52ebc3f9224210078fa5b7881dcb
Raphael Vacari <raphaelvacari7@gmail.com>
2025-09-17 12:50:17 -0300

    bean validation

--- src/main/java/com/devsuperior/rvcommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/rvcommerce/dto/ProductDTO.java
@@ -1,13 +1,24 @@
 package com.devsuperior.rvcommerce.dto;
 
 import com.devsuperior.rvcommerce.entities.Product;
-import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
     private Long id;
+
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 e 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+
+    @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+
+    @Positive(message = "O preço precisa ser positivo")
     private Double price;
     private String imgUrl;
 
