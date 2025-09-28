67cef0f3a4de748b807ed642c1e2f05b9a70839a
Raphael Vacari <raphaelvacari7@gmail.com>
2025-09-28 16:07:39 -0300

    DTOs

---
+++ src/main/java/com/devsuperior/rvcommerce/dto/CategoryDTO.java
@@ -0,0 +1,27 @@
+package com.devsuperior.rvcommerce.dto;
+
+import com.devsuperior.rvcommerce.entities.Category;
+
+public class CategoryDTO {
+
+    private Long id;
+    private String name;
+
+    public CategoryDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public CategoryDTO(Category entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
